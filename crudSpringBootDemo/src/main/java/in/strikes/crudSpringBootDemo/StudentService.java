5426db819b9e14a009c63f4eebe3693ae241a53d
Ashmit <ashmit.rana2031@gmail.com>
2026-07-11 07:41:02 +0530

    CRUD in SpringBoot

---
+++ crudSpringBootDemo/src/main/java/in/strikes/crudSpringBootDemo/StudentService.java
@@ -0,0 +1,14 @@
+package in.strikes.crudSpringBootDemo;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class StudentService {
+    // 1. End point listen (/app/students POST)
+
+    // 2. Business logic
+
+    // 3. Interact with DB to store
+
+    // 4. Response back to client (POSTMAN)
+}
